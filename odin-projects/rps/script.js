6fa4709ee6d50cb62ccabbba6e11630c78c35317
dannetzy <mdannefebrian@gmail.com>
2025-11-11 09:55:46 +0700

    update rps

--- odin-projects/rps/script.js
+++ odin-projects/rps/script.js
@@ -11,28 +11,31 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const choice = prompt("play rock paper scissors");
+  const choice = prompt("play rock paper scissors").toLowerCase();
   if (!choice) {
     alert("thanks for playing (or not) rock paper scissors!");
     return;
   }
   if (choice === 'rock' || choice === 'paper' || choice === 'scissor') {
-    return choice.toLowerCase();
+    return choice;
   }
   alert("please enter a valid keyword (input blank or cancel to abort the game)sci");
   getHumanChoice();
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
+/* returns true if player A wins
+  returns false if player B wins
+  returns undefined if tie
+  returns 0 if choiceA is undefined (ie. game aborted) */
+function playRound(choiceA, choiceB) {
+/* old code
+  if (a === b) {
     console.log("Tie!");
-    computerScore++; 
-    humanScore++;
     return;
   }
-  switch (humanChoice) {
+  switch (a) {
     case "rock":
-      switch (computerChoice) {
+      switch (b) {
         case "paper":
           console.log("You lose! Rock gets beaten by paper.")
           computerScore++;
@@ -44,7 +47,7 @@ function playRound(humanChoice, computerChoice) {
       }
       break;
     case "paper":
-      switch (computerChoice) {
+      switch (b) {
         case "rock":
           console.log("You win! Paper beats rock.")
           humanScore++;
@@ -56,7 +59,7 @@ function playRound(humanChoice, computerChoice) {
       }
       break;
     case "scissor":
-      switch (computerChoice) {
+      switch (b) {
         case "rock":
           console.log("You lose! Scissor gets beaten by rock.")
           computerScore++;
@@ -67,31 +70,96 @@ function playRound(humanChoice, computerChoice) {
           break;
       }
       break;
+  }*/
+  if (choiceA === undefined) {
+    return 0;
+  }
+  if (choiceA === choiceB) {
+    return;
+  }
+  switch (choiceA) {
+    case "rock":
+      switch (choiceB) {
+        case "paper":
+          return false;
+        case "scissor":
+          return true;
+      }
+      break;
+    case "paper":
+      switch (choiceB) {
+        case "rock":
+          return true;
+        case "scissor":
+          return false;
+      }
+      break;
+    case "scissor":
+      switch (choiceB) {
+        case "rock":
+          return false;
+        case "paper":
+          return true;
+      }
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
+function playGameHTML(times) {
+  const gameResultElement = document.querySelector(".game-result");
+
+  //resets
+  computerScore = 0;
+  humanScore = 0;
+  gameResultElement.innerHTML = '';
+
+  for (let i = 0; i < times; i++) {
+    const gameResult = playRound(getHumanChoice(), getComputerChoice());
+
+    if (gameResult === 0) {
+      gameResultElement.innerHTML += "<p>Game aborted.</p>";
+      continue;
+    }
+    if (gameResult) {
+      gameResultElement.innerHTML += "<p>Human wins!</p>";
+      humanScore++;
+    } else if (gameResult === false) {
+      gameResultElement.innerHTML += "<p>Computer wins!</p>";  
+      computerScore++;
+    } else {
+      gameResultElement.innerHTML += "<p>Tie!</p>";
+    }
   }
-  if (humanScore > computerScore) {
-    console.log(`Human wins! ${humanScore}:${computerScore}`);
-  } else if (humanScore == computerScore) {
-    console.log("tie!")
-  } else {
-    console.log(`Computer wins! ${humanScore}:${computerScore}`);
-  }
+  gameResultElement.innerHTML += `${humanScore > computerScore ? "Human wins!" : humanScore === computerScore ? "Tie" : "Computer wins!"} Score: ${humanScore}:${computerScore}.`;
 }
+
+function playGame(times) {
+  //resets
+  computerScore = 0;
+  humanScore = 0;
+
+  for (let i = 0; i < times; i++) {
+    const gameResult = playRound(getHumanChoice(), getComputerChoice());
+
+    console.log(gameResult ? "Human wins!" : gameResult === false ? "Computer wins!" : "Tie!");
+    gameResult ? humanScore++ : gameResult === false ? computerScore++ : "tie (this code does nothing)";
+  }
+  console.log(`${humanScore > computerScore ? "Human wins!" : humanScore === computerScore ? "Tie!" : "Computer wins!"} ${humanScore}:${computerScore}`);
+}
+
+document.querySelector(".rps").addEventListener("click", () => {
+  playGameHTML(5);
+});
+
 /*
 paper paper     \
 rock rock        > Tie!
 scissor scissor /
 
-paper scissor: B
-rock paper: B
-scissor rock: B
+paper scissor: B win
+rock paper: B win
+scissor rock: B win
 
-scissor paper: A
-paper rock: A
-rock scissor: A
+scissor paper: A win
+paper rock: A win
+rock scissor: A win
 */
